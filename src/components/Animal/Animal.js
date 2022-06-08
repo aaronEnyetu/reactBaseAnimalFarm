cf112fa60cd5f31ea672fff7677d78194fc5b855
Aaron Enyetu <aaronenyetu@gmail.com>
2022-06-07 17:39:00 -0700

    Displaying animals working

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './Animal.css';
 
 
 export default function Animal({ name, type, says }) {
-  return (<>
-    <p> {name}</p>
-    <p> {type}</p>
-    <p> {says} </p>
-  </>);
+  return (
+    <div className= "animal-div">
+      <p> {name}</p>
+      <img alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
+
+      <p> {type}</p>
+      <p> {says} </p>
+      
+    </div>);
 }
